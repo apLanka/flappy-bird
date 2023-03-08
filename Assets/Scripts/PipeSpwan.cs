1f32c087f9f1cc96e94e8e26a4f8a5c4c4b050f6
Pasindu Lanka <106298729+apLanka@users.noreply.github.com>
2023-03-08 08:36:35 +0530

    add obstacles random position script

--- Assets/Scripts/PipeSpwan.cs
+++ Assets/Scripts/PipeSpwan.cs
@@ -5,14 +5,31 @@ using UnityEngine;
 public class PipeSpwan : MonoBehaviour
 {
     public GameObject pipes;
+
+    public float timeSpwan;
+    public float startTimeSpwan;
+    private float yPos;
+
+    public Vector3 spwanPos;
     void Start()
     {
-        Instantiate(pipes, transform.position, transform.rotation);
+        
     }
 
     
     void Update()
     {
+        yPos = Random.Range(-2.45f, 2.15f);
+        spwanPos = new Vector3(transform.position.x, yPos, transform.position.z);
         
+        if (timeSpwan <= 0)
+        {
+            Instantiate(pipes, spwanPos, transform.rotation);
+            timeSpwan = startTimeSpwan;
+        }
+        else
+        {
+            timeSpwan -= Time.deltaTime;
+        }
     }
 }
