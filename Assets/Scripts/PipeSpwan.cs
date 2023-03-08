eabfbeac11fd44e5cb381dec941f35e88f243576
Pasindu Lanka <106298729+apLanka@users.noreply.github.com>
2023-03-08 08:13:27 +0530

    add obstacles

---
+++ Assets/Scripts/PipeSpwan.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeSpwan : MonoBehaviour
+{
+    public GameObject pipes;
+    void Start()
+    {
+        Instantiate(pipes, transform.position, transform.rotation);
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Pipes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pipes : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb;
+    
+    void Update()
+    {
+        transform.Translate(Vector3.left * speed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Endpipe"))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
