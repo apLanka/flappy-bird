b0bf76023fdb1f692e354e34a1e639106f4faaae
Pasindu Lanka <106298729+apLanka@users.noreply.github.com>
2023-03-08 09:30:30 +0530

    add main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Gameplay");
+    }
+
+    public void Credit()
+    {
+        
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
